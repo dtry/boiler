106c1be1f6011b55e4ea52965549ed0b7d559c43
dmitry <dkagansky@gmail.com>
2018-01-03 22:44:28 +0300

    configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const proxy = require('express-http-proxy');
+const path = require('path');
+
+app.use(express.static('../dist'));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -24,6 +27,8 @@ app.get('/search/tracks', proxy('https://api-v2.soundcloud.com', {
   }
 }));
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
